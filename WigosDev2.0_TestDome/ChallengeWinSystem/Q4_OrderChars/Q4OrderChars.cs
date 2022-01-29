fe3337edbbe32b3c30ed198e3cd74b90a9e73501
Lucas Vargas <lucasjv92@gmail.com>
2022-01-29 10:00:08 -0300

    Upload q4 order chars

---
+++ WigosDev2.0_TestDome/ChallengeWinSystem/Q4_OrderChars/Q4OrderChars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeWinSystem.Q4_OrderChars
+{
+    public class Q4OrderChars
+    {
+        public static IEnumerable<char> OrderChars(params string[] strings)
+        {
+            return strings.SelectMany(str => str.ToCharArray())
+                .Where(chr => Char.IsLetter(chr)).OrderBy(chr => chr);
+        }
+    }
+}
