f8c3eb2e651004be4c41036676e01ab3175f459a
Lucas Vargas <lucasjv92@gmail.com>
2022-01-30 10:08:01 -0300

    Upload Q4 OrderChars with tests

--- WigosDev2.0_TestDome/ChallengeWinSystem/Q4_OrderChars/Q4OrderChars.cs
+++ WigosDev2.0_TestDome/ChallengeWinSystem/Q4_OrderChars/Q4OrderChars.cs
@@ -8,6 +8,14 @@ namespace ChallengeWinSystem.Q4_OrderChars
 {
     public class Q4OrderChars
     {
+        /*
+         * Select all acceptable answers
+         * a) OrerChars ("a", "b").ToArray() will return ['b', 'a']
+         * b) OrerChars ("ab", "ab").ToArray() will return ['a', 'b']
+         * c) OrerChars ("b1", "a0").ToArray() will return ['a', 'b']
+         * d) Sorting will be deferred until the results are iterated over to List/ ToArray is called
+         * La ordenación se aplazará hasta que los resultados se iteren a List/ToArray se llama
+         */
         public static IEnumerable<char> OrderChars(params string[] strings)
         {
             return strings.SelectMany(str => str.ToCharArray())
---
+++ WigosDev2.0_TestDome/Tests/OrderCharsTests.cs
@@ -0,0 +1,38 @@
+﻿using ChallengeWinSystem.Q4_OrderChars;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUtils
+{
+    [TestClass]
+    public class OrderCharsTests
+    {
+        [TestMethod]
+        public void OrderCharsTest1()
+        {
+            var chars = Q4OrderChars.OrderChars("a", "b").ToArray();
+            Assert.AreEqual('a', chars[0]);
+            Assert.AreEqual('b', chars[1]);
+        }
+
+        [TestMethod]
+        public void OrderCharsTest2()
+        {
+            var chars = Q4OrderChars.OrderChars("ab", "ab").ToArray();
+            Assert.AreEqual('a', chars[0]);
+            Assert.AreEqual('a', chars[1]);
+        }
+
+        [TestMethod]
+        public void OrderCharsTest3()
+        {
+            var chars = Q4OrderChars.OrderChars("b1", "a0").ToArray();
+            Assert.AreEqual('a', chars[0]);
+            Assert.AreEqual('b', chars[1]);
+        }
+    }
+}
